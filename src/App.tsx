445ab4f0798d18a03ed745c8c83693eca61663bc
Oliver Brunovsky <obrunovsky7@gmail.com>
2023-02-20 07:25:02 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import "./App.css";
 import About from "./Components/About";
